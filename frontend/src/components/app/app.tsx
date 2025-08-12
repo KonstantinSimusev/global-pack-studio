dcc4f7acc9c8cc27f76e26747074179a9189d76f
Konstantin Simusev <simussev.k@icloud.com>
2025-08-12 23:33:48 +0500

    Add fonts and styles

---
+++ frontend/src/components/app/app.tsx
@@ -0,0 +1,31 @@
+import styles from "./app.module.css";
+
+// import { Header } from '../header/header';
+// import { Cover } from '../cover/cover';
+
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import gpsLogo from '/icon.svg';
+
+// const initialData = {
+//   date: '12 августа 2025 года',
+//   shifty: 2,
+//   team: 3
+// }
+
+const App = () => {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div className={styles.container}>
+      <br />
+      <p className={styles.title_m}>Global Pack Studio</p>
+      <br />
+      <p className={styles.title_c}>Global Pack Studio</p>
+      {/* <Header />
+      <Cover {...initialData} /> */}
+    </div>
+  );
+};
+
+export default App;
