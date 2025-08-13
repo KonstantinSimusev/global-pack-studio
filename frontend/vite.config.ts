8944a3b351e7bcf1f74b096aaa423c7648cd224d
Konstantin Simusev <simussev.k@icloud.com>
2025-08-13 08:02:35 +0500

    Add html plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import autoprefixer from 'autoprefixer';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+      }
+    })
+  ],
+
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
+  build: {
+    minify: true,
+    cssCodeSplit: false,
+  },
+});
