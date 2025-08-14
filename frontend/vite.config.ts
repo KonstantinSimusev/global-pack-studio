430522c9470b10b9d01b9805f4c38e7beb49f2f4
Konstantin Simusev <simussev.k@icloud.com>
2025-08-14 21:46:57 +0500

    Fix vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import autoprefixer from 'autoprefixer';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production'
-    ? '/global-pack-studio/'
-    : '/',
-  css: {
-    postcss: {
-      plugins: [autoprefixer()],
-    },
-  },
-  build: {
-    outDir: 'dist',
-    minify: true,
-    cssCodeSplit: false,
-  },
+  base: process.env.NODE_ENV === 'production' ? '/global-pack-studio/' : '/',
 });
